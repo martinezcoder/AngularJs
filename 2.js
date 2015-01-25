e665d5d180d263cf28dd05e86c9527e1e39014d8
Fran J Martinez <martinezcoder@gmail.com>
2015-01-25 21:05:00 +0100

    code updated to angular 1.3.10

--- 2.js
+++ 2.js
@@ -1,25 +1,32 @@
-function TodoCtrl($scope) {
+angular.module('todoApp', [])
+  .controller('TodoController', ['$scope', function($scope) {
+    $scope.todos = [
+      {text:'Learn AngularJS', done:false},
+      {text:'Build an app', done:false}
+    ];
 
-  $scope.todos = [
-    {text:'Learn AngularJS', done:false}, 
-    {text:'Build an app', done:false}
-  ];
+    $scope.addTodo = function() {
+      if (($scope.todoText != '')&&($scope.todoText != null)) {
+        $scope.todos.push({text:$scope.todoText, done:false});
+        $scope.todoText = '';
+      }
+    };
 
-  $scope.getTotalTodos = function () {
-    return $scope.todos.length;
-  };
+    $scope.remaining = function () {
+      var count = 0;
+      angular.forEach($scope.todos, function(todo) {
+        count += todo.done ? 0 : 1;
+      });
+      return count;
+    };
 
-  $scope.clearCompleted = function() {
-    var oldTodos = $scope.todos;
-    $scope.todos = [];
-    angular.forEach(oldTodos, function(todo) {
-      if (!todo.done) $scope.todos.push(todo);
-    });
-  };
+    $scope.archive = function() {
+      var oldTodos = $scope.todos;
+      $scope.todos = [];
+      angular.forEach(oldTodos, function(todo) {
+        if (!todo.done) $scope.todos.push(todo);
+      });
+    };
 
-  $scope.addTodo = function() {
-    $scope.todos.push({text: $scope.formTodoText, done: false});
-    $scope.formTodoText = '';
-  };
-}
+}]);
 
