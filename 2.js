e76faeb24ad2d4e59e21cb1a8fec1a890ef0c623
Fran J Martinez <martinezcoder@gmail.com>
2015-01-25 20:10:17 +0100

    basic and controls

---
+++ 2.js
@@ -0,0 +1,25 @@
+function TodoCtrl($scope) {
+
+  $scope.todos = [
+    {text:'Learn AngularJS', done:false}, 
+    {text:'Build an app', done:false}
+  ];
+
+  $scope.getTotalTodos = function () {
+    return $scope.todos.length;
+  };
+
+  $scope.clearCompleted = function() {
+    var oldTodos = $scope.todos;
+    $scope.todos = [];
+    angular.forEach(oldTodos, function(todo) {
+      if (!todo.done) $scope.todos.push(todo);
+    });
+  };
+
+  $scope.addTodo = function() {
+    $scope.todos.push({text: $scope.formTodoText, done: false});
+    $scope.formTodoText = '';
+  };
+}
+
